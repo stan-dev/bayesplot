2cbd2f7057d490e6a189430a21234dfb06a02049
jgabry <jgabry@gmail.com>
2016-05-21 14:55:03 -0400

    update details doc template

--- man-roxygen/details-ppc.R
+++ man-roxygen/details-ppc.R
@@ -1,3 +1,3 @@
-#' @details For binomial (not Bernoulli) data, \code{y} and \code{yrep} should 
-#'   contain the "success" proportions (not "success" or "failure" counts). For 
-#'   ordinal data, \code{y} and \code{yrep} should both contain integers.
+#' @details For binomial (not Bernoulli) data, the plots will be most useful if
+#'   \code{y} and \code{yrep} contain the "success" proportions (not "success"
+#'   or "failure" counts).
