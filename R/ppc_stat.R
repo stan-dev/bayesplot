07940a5f231c6a0e4578f6d4121a47142c35535f
jgabry <jgabry@gmail.com>
2016-05-21 14:34:24 -0400

    reformatting

--- R/ppc_stat.R
+++ R/ppc_stat.R
@@ -41,10 +41,13 @@ ppc_stat <- function(y, yrep, stat = "mean", ...) {
   validate_y_and_yrep(y, yrep)
   stopifnot(is.character(stat), length(stat) == 1)
 
+  stat1 <- match.fun(stat)
+  T_y <- stat1(y)
+  T_yrep <- apply(yrep, 1, stat1)
+
   vline_color <- .PP_DARK
   fill_color <- .PP_LIGHT
   outline_color <- .PP_LIGHT_highlight
-
   defaults <- list(
     fill = fill_color,
     color = outline_color,
@@ -53,28 +56,28 @@ ppc_stat <- function(y, yrep, stat = "mean", ...) {
   )
   geom_args <- set_geom_args(defaults, ...)
   geom_args$mapping <- aes_string(y = "..density..")
-  geom_args$show.legend <- FALSE
 
-  stat1 <- match.fun(stat)
-  T_y <- stat1(y)
-  T_yrep <- apply(yrep, 1, stat1)
-  base <- ggplot(data.frame(x = T_yrep), aes_string(x = "x", color = "'A'"))
-  graph <- base +
-    call_geom("histogram", geom_args) +
-    geom_vline(
-      data = data.frame(t = T_y),
-      mapping = aes_string(xintercept = "t", color = "factor(t)"),
-      size = 2,
-      show.legend = TRUE
-    ) +
-    scale_color_manual(
-      name = "",
-      values = c(vline_color, fill_color),
-      labels = c("T(y)", "T(yrep)")
-    ) +
-    xlab(paste("Stat =", stat))
-
-  graph + theme_ppc(legend_position = "right")
+  ggplot(
+    data = data.frame(x = T_yrep),
+    mapping = aes_string(x = "x", color = "'A'")
+  ) +
+  call_geom("histogram", geom_args) +
+  geom_vline(
+    data = data.frame(t = T_y),
+    mapping = aes_string(xintercept = "t", color = "factor(t)"),
+    size = 2,
+    show.legend = TRUE
+  ) +
+  scale_color_manual(
+    name = "",
+    values = c(vline_color, fill_color),
+    labels = c("T(y)", "T(yrep)")
+  ) +
+  xlab(paste("Stat =", stat)) +
+  theme_ppc(
+    y_text = FALSE,
+    legend_position = "right"
+  )
 }
 
 #' @export
@@ -84,6 +87,13 @@ ppc_stat_2d <- function(y, yrep, stat = c("mean", "sd"), ...) {
   validate_y_and_yrep(y, yrep)
   stopifnot(is.character(stat), length(stat) == 2)
 
+  stat1 <- match.fun(stat[1])
+  stat2 <- match.fun(stat[2])
+  T_y1 <- stat1(y)
+  T_y2 <- stat2(y)
+  T_yrep1 <- apply(yrep, 1, stat1)
+  T_yrep2 <- apply(yrep, 1, stat2)
+
   defaults <- list(
     shape = 21,
     size = 2,
@@ -92,52 +102,49 @@ ppc_stat_2d <- function(y, yrep, stat = c("mean", "sd"), ...) {
   )
   geom_args <- set_geom_args(defaults, ...)
 
-  if (is.character(stat[1]))
-    stat1 <- match.fun(stat[1])
-  if (is.character(stat[2]))
-    stat2 <- match.fun(stat[2])
-  T_y1 <- stat1(y)
-  T_y2 <- stat2(y)
-  T_yrep1 <- apply(yrep, 1, stat1)
-  T_yrep2 <- apply(yrep, 1, stat2)
-
-  base <- ggplot(
+  ggplot(
     data = data.frame(x = T_yrep1, y = T_yrep2),
     mapping = aes_string(x = "x", y = "y")
+  ) +
+  call_geom("point", geom_args) +
+  annotate(
+    geom = "segment",
+    x = c(T_y1,-Inf),
+    xend = c(T_y1, T_y1),
+    y = c(-Inf, T_y2),
+    yend = c(T_y2, T_y2),
+    color = .PP_DARK_highlight,
+    linetype = 2,
+    size = 0.5
+  ) +
+  geom_point(
+    data = data.frame(x = T_y1, y = T_y2),
+    mapping = aes_string(
+      x = "x",
+      y = "y",
+      fill = "'Ty'",
+      color = "'Ty'"
+    ),
+    size = 4,
+    shape = 21,
+    stroke = 1
+  ) +
+  scale_fill_manual(
+    name = "",
+    values = c('Ty' = .PP_DARK),
+    labels = c('Ty' = "T(y)")
+  ) +
+  scale_color_manual(
+    name = "",
+    values = c('Ty' = .PP_DARK_highlight),
+    labels = c('Ty' = "T(y)")
+  ) +
+  labs(
+    x = paste("Stat =", stat[1]),
+    y = paste("Stat =", stat[2])
+  ) +
+  theme_ppc(
+    y_text = TRUE,
+    legend_position = "right"
   )
-  graph <- base +
-    call_geom("point", geom_args) +
-    annotate(
-      geom = "segment",
-      x = c(T_y1, -Inf),
-      xend = c(T_y1, T_y1),
-      y = c(-Inf, T_y2),
-      yend = c(T_y2, T_y2),
-      color = .PP_DARK_highlight,
-      linetype = 2,
-      size = 0.5
-    ) +
-    geom_point(
-      data = data.frame(x = T_y1, y = T_y2),
-      mapping = aes_string(x = "x", y = "y", fill = "'Ty'", color = "'Ty'"),
-      size = 4,
-      shape = 21,
-      stroke = 1
-    ) +
-    scale_fill_manual(
-      name = "",
-      values = c('Ty' = .PP_DARK),
-      labels = c('Ty' = "T(y)")
-    ) +
-    scale_color_manual(
-      name = "",
-      values = c('Ty' = .PP_DARK_highlight),
-      labels = c('Ty' = "T(y)")
-    ) +
-    labs(
-      x = paste("Stat =", stat[1]),
-      y = paste("Stat =", stat[2])
-    )
-
-  graph + theme_ppc(y_text = TRUE, legend_position = "right")
 }
