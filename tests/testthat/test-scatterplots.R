129d2a03a1165555f58ce6611d14564ac4440095
jgabry <jgabry@gmail.com>
2016-05-23 16:14:14 -0400

    update tests

--- tests/testthat/test-scatterplots.R
+++ tests/testthat/test-scatterplots.R
@@ -2,6 +2,16 @@ library(ppcheck)
 context("scatterplots")
 
 test_that("ppc_scatter returns ggplot object", {
-  expect_gg(ppc_scatter_average(y, yrep))
+  expect_gg(ppc_scatter(y, yrep[1,, drop = FALSE]))
   expect_gg(ppc_scatter(y, yrep[1:3, ]))
+})
+
+test_that("ppc_scatter_avg returns ggplot object", {
+  expect_gg(ppc_scatter_avg(y, yrep))
+})
+
+test_that("ppc_scatter_avg_grouped returns a ggplot object", {
+  expect_gg(ppc_scatter_avg_grouped(y, yrep, group))
+  expect_gg(ppc_scatter_avg_grouped(y, yrep, as.numeric(group)))
+  expect_gg(ppc_scatter_avg_grouped(y, yrep, as.integer(group)))
 })
