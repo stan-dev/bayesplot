b3dfe76fa71bbc09fabc84fe52e921dc0e0a46b4
jgabry <jgabry@gmail.com>
2016-05-22 21:10:47 -0400

    add test and data for ppc_resid_binned

--- tests/testthat/test-residuals.R
+++ tests/testthat/test-residuals.R
@@ -4,3 +4,8 @@ context("residuals")
 test_that("ppc_resid returns ggplot object", {
   expect_gg(ppc_resid(y, yrep[1:5, ]))
 })
+
+test_that("ppc_resid_binned returns ggplot object", {
+  load("data-for-binomial.rda")
+  expect_gg(ppc_resid_binned(y, Ey))
+})
