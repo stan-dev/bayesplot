9579cd60598d0233b85cff6e1ea13ad91b139172
jgabry <jgabry@gmail.com>
2016-05-22 21:53:20 -0400

    test more cases for ppc_resid_binned

--- tests/testthat/test-residuals.R
+++ tests/testthat/test-residuals.R
@@ -3,9 +3,12 @@ context("residuals")
 
 test_that("ppc_resid returns ggplot object", {
   expect_gg(ppc_resid(y, yrep[1:5, ]))
+  expect_gg(ppc_resid(y, yrep[1,, drop = FALSE]))
 })
 
 test_that("ppc_resid_binned returns ggplot object", {
   load("data-for-binomial.rda")
   expect_gg(ppc_resid_binned(y, Ey))
+  expect_gg(ppc_resid_binned(y[1:5], Ey[, 1:5]))
+  expect_gg(ppc_resid_binned(rep(y, 2), cbind(Ey, Ey)))
 })
