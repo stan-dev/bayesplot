e04488806db348e27c0db4b0beb11136abcef7e5
jgabry <jgabry@gmail.com>
2016-05-24 16:07:36 -0400

    specify y and yrep columns need to be in same order

--- man-roxygen/args-y-yrep.R
+++ man-roxygen/args-y-yrep.R
@@ -2,5 +2,6 @@
 #' @param yrep An \eqn{S} by \eqn{N} matrix of draws from the posterior
 #'   predictive distribution, where \eqn{S} is the size of the posterior sample
 #'   (or subset of the posterior sample used to generate \code{yrep}) and
-#'   \eqn{N} is the number of observations (the length of \code{y}).
-#'   See Details for additional instructions.
+#'   \eqn{N} is the number of observations (the length of \code{y}). The columns
+#'   of \code{yrep} should be in the same order as the data points in \code{y}
+#'   for the plots to make sense. See Details for additional instructions.
