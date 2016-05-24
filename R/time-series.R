5e2c9ab7c53905ef4a01797d9895791a4a88f339
jgabry <jgabry@gmail.com>
2016-05-24 16:18:59 -0400

    ppc_ts: add y to plot and add times argument
    @paul-buerkner I got rid of ppc_ts_grouped (just temporarily until I
    fix it), but how does ppc_ts look to you now?

--- R/time-series.R
+++ R/time-series.R
@@ -1,7 +1,8 @@
 #' PPCs for time series
 #'
-#' Central interval estimates (quantiles) of \eqn{y^{rep}}{yrep} by time and,
-#' optionally, levels of a grouping variable, with \eqn{y} points overlaid.
+#' Central interval estimates (quantiles) of \eqn{y^{rep}}{yrep} by time,
+#' with \eqn{y} overlaid.
+#'
 #'
 #' @name time-series
 #' @family PPCs
@@ -17,80 +18,113 @@
 #'
 #' @examples
 #' y <- rnorm(50)
-#' yrep <- matrix(rnorm(5000), ncol = 50)
-#' group <- gl(5, 10)
+#' yrep <- matrix(rnorm(5000, 0, 2), ncol = 50)
 #' ppc_ts(y, yrep)
-#' ppc_ts_grouped(y, yrep, group)
+#'
+#' time <- sample(200, size = 50)
+#' ppc_ts(y, yrep, time)
+#'
 #'
 NULL
 
 #' @rdname time-series
 #' @export
+#' @param time An optional numeric vector, the same length as \eqn{y}, of
+#'   \emph{unique} time values. If \code{time} is missing then it is set to
+#'   \code{1:length(y)}.
 #' @param prob A value between 0 and 1 indicating the desired probability mass
 #'   to include in the \code{yrep} intervals. The default is 0.8.
+#' @param y_style Should \code{y} be plotted as points connected by lines, only
+#'   the points, or only the lines?
 #'
-ppc_ts <- function(y, yrep, ..., prob = 0.8) {
+ppc_ts <- function(y,
+                   yrep,
+                   time,
+                   ...,
+                   prob = 0.8,
+                   y_style = c("both", "points", "lines")) {
   y <- validate_y(y)
   yrep <- validate_yrep(yrep, y)
-  ppc_time_series(y, yrep, prob = prob, size = 2)
-}
+  time <- validate_time(time, y)
 
-#' @rdname time-series
-#' @export
-#' @template args-group
-#'
-ppc_ts_grouped <- function(y, yrep, group, ..., prob = 0.8) {
-  y <- validate_y(y)
-  yrep <- validate_yrep(yrep, y)
-  group <- validate_group(group, y)
-  ppc_time_series(y, yrep, group, prob = prob, size = 1) +
-    facet_wrap("group", scales = "free", labeller = label_both)
-}
+  y_style <- match.arg(y_style)
 
-
-ppc_time_series <- function(y, yrep, group, prob = 0.8, ...) {
-  d <- melt_yrep(yrep)
-  if (missing(group)) {
-    dots <- list(~y_id)
-  } else {
-    d$group <- group
-    dots <- list(~y_id, ~group)
-  }
-  molten_d <- dplyr::group_by_(d, .dots = dots)
+  molten_d <- ppc_dist_data(y, yrep)
+  molten_d <- dplyr::rename_(molten_d, .dots = setNames(list(~y_id), "time"))
+  molten_d <- dplyr::arrange_(molten_d, .dots = list(~rep_id, ~time))
+  molten_d$time <- time
+  grouped_d <- dplyr::group_by_(molten_d, .dots = list(~time, ~is_y))
   alpha <- (1 - prob) / 2
   probs <- c(alpha, 1 - alpha)
   plot_data <- dplyr::summarise_(
-    .data = molten_d,
+    .data = grouped_d,
     .dots = list(
-      median = ~median(value),
-      lower = ~quantile(value, prob = probs[1]),
-      upper = ~quantile(value, prob = probs[2])
+      median = ~ median(value),
+      lower = ~ quantile(value, prob = probs[1]),
+      upper = ~ quantile(value, prob = probs[2])
     )
   )
+  pd_y <- plot_data[plot_data$is_y, , drop = FALSE]
+  pd_yrep <- plot_data[!plot_data$is_y, , drop = FALSE]
 
   scheme <- get_color_scheme()
-  ggplot(
-    data = plot_data,
+  graph <- ggplot(
+    data = pd_yrep,
     mapping = aes_string(
-      x = "y_id",
+      x = "time",
       y = "median",
       ymin = "lower",
       ymax = "upper"
     )
   ) +
-    geom_smooth(
-      stat = "identity",
-      fill = scheme[["light"]],
-      color = scheme[["light_highlight"]]
-    ) +
-    geom_line(color = scheme[["mid"]]) +
+    geom_smooth(stat = "identity",
+                fill = scheme[["light"]],
+                color = scheme[["light_highlight"]])
+
+  if (y_style %in% c("both", "lines"))
+    graph <- graph +
+      geom_line(
+        data = pd_y,
+        color = "black", # scheme[["mid"]],
+        size = 0.25
+      )
+
+  if (y_style %in% c("both", "points"))
+    graph <- graph +
     geom_point(
+      data = pd_y,
       shape = 21,
       fill = scheme[["dark"]],
       color = scheme[["dark_highlight"]],
-      ...
-    ) +
+      size = 1
+    )
+
+  graph +
     labs(x = "Time", y = yrep_label()) +
     coord_cartesian(expand = FALSE) +
     theme_ppc()
 }
+
+validate_time <- function(time, y) {
+  if (missing(time)) {
+    time <- 1:length(y)
+  } else {
+    stopifnot(is.numeric(time),
+              identical(length(time), length(y)),
+              identical(length(time), length(unique(time))))
+  }
+  time
+}
+
+
+# @rdname time-series
+# @export
+# @template args-group
+#
+# ppc_ts_grouped <- function(y, yrep, group, ..., prob = 0.8) {
+#   y <- validate_y(y)
+#   yrep <- validate_yrep(yrep, y)
+#   group <- validate_group(group, y)
+#   ppc_time_series(y, yrep, group, prob = prob, size = 1) +
+#     facet_wrap("group", scales = "free", labeller = label_both)
+# }
--- tests/testthat/test-time-series.R
+++ tests/testthat/test-time-series.R
@@ -4,9 +4,13 @@ context("time-series")
 test_that("ppc_ts returns ggplot object", {
   expect_gg(ppc_ts(y, yrep))
   expect_gg(ppc_ts(y, yrep, prob = 0.5))
+  expect_gg(ppc_ts(y, yrep, y_style = "points"))
+  expect_gg(ppc_ts(y, yrep, y_style = "lines"))
+  expect_gg(ppc_ts(y, yrep, times = 1:length(y)))
+  expect_gg(ppc_ts(y, yrep, times = seq(1, 2 * length(y), by = 2)))
 })
 
-test_that("ppc_ts_grouped returns ggplot object", {
-  expect_gg(ppc_ts_grouped(y, yrep, group))
-  expect_gg(ppc_ts_grouped(y, yrep, as.factor(group), prob = 0.9))
-})
+# test_that("ppc_ts_grouped returns ggplot object", {
+#   expect_gg(ppc_ts_grouped(y, yrep, group))
+#   expect_gg(ppc_ts_grouped(y, yrep, as.factor(group), prob = 0.9))
+# })
