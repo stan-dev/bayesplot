717f20e41336057743b9ce8130a2067a2828a2a5
jgabry <jgabry@gmail.com>
2016-05-27 17:21:59 -0400

    more color scheme tests

--- tests/testthat/test-aesthetics.R
+++ tests/testthat/test-aesthetics.R
@@ -1,7 +1,7 @@
 library(ppcheck)
 context("schemes and themes")
 
-test_that("getting and setting color scheme works", {
+test_that("getting and setting the color scheme works", {
   expect_identical(get_color_scheme(), scheme_reds())
   set_color_scheme("blues")
   expect_identical(get_color_scheme(), scheme_blues())
@@ -11,6 +11,15 @@ test_that("getting and setting color scheme works", {
   expect_identical(get_color_scheme(), scheme_greys())
   set_color_scheme("purples")
   expect_identical(get_color_scheme(), scheme_purples())
+})
+
+test_that("ppc_color returns correct color values", {
+  scheme <- set_color_scheme("greens")
+  levs <- scheme_level_names()
+
+  expect_identical(ppc_color(levs), unlist(scheme[levs], use.names = FALSE))
+  for (lev in levs)
+    expect_identical(ppc_color(lev), scheme[[lev]], info = lev)
 })
 
 test_that("theme_ppc creates ggplot theme", {
