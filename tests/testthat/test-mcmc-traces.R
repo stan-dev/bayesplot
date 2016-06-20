36688c32eb5eb4af01ab762b9174614c59264aab
jgabry <jgabry@gmail.com>
2016-06-20 15:03:08 -0400

    test mcmc_trace with n_warmup and window

--- tests/testthat/test-mcmc-traces.R
+++ tests/testthat/test-mcmc-traces.R
@@ -9,10 +9,12 @@ test_that("mcmc_trace returns a ggplot object", {
   expect_gg(mcmc_trace(mat))
   expect_gg(mcmc_trace(dframe))
   expect_gg(mcmc_trace(dframe_multiple_chains))
+  expect_gg(mcmc_trace(chainlist))
 
   expect_gg(mcmc_trace(arr1))
   expect_gg(mcmc_trace(mat1))
   expect_gg(mcmc_trace(dframe1))
+  expect_gg(mcmc_trace(chainlist1))
 })
 
 # functions that require multiple chains ----------------------------------
@@ -25,4 +27,15 @@ test_that("mcmc_trace_highlight throws error if 1 chain but multiple chains requ
   expect_error(mcmc_trace_highlight(mat), "requires multiple")
   expect_error(mcmc_trace_highlight(dframe, highlight = 1), "requires multiple chains")
   expect_error(mcmc_trace_highlight(arr1chain, highlight = 1), "requires multiple chains")
+})
+
+# options -----------------------------------------------------------------
+test_that("mcmc_trace options work", {
+  expect_gg(g1 <- mcmc_trace(arr, regex_pars = "beta", window = c(5, 10)))
+  coord <- g1$coordinates
+  expect_equal(g1$coordinates$limits$x, c(5, 10))
+
+  expect_gg(g2 <- mcmc_trace(arr, regex_pars = "beta", n_warmup = 10))
+  ll <- g2$labels
+  expect_true(all(c("xmin", "xmax", "ymin", "ymax") %in% names(ll)))
 })
