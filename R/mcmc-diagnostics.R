35ceb737fadd9f95371a503f90cae4a13e464f1b
jgabry <jgabry@gmail.com>
2016-07-23 18:34:20 -0400

    mcmc_rhat_dots

--- R/mcmc-diagnostics.R
+++ R/mcmc-diagnostics.R
@@ -21,6 +21,17 @@
 #'
 #' @template seealso-color-scheme
 #'
+#' @examples
+#'
+#' # fake rhat values to use for demonstration
+#' rhat <- c(runif(100, 1, 1.15))
+#' mcmc_rhat_hist(rhat)
+#' mcmc_rhat_dots(rhat)
+#'
+#' set_color_scheme("blue")
+#' mcmc_rhat_dots(runif(1000, 1, 1.3))
+#' mcmc_rhat_dots(runif(1000, 1, 1.07)) + ggplot2::ylim(1,1.1)
+#'
 NULL
 
 #' @rdname MCMC-diagnostics
@@ -32,7 +43,7 @@ mcmc_rhat_hist <- function(rhat, ..., binwidth = NULL) {
     data.frame(x = rhat, lev = factor_rhat(rhat)),
     aes_(
       x = ~ x,
-      y = ~ ..density..,
+      # y = ~ ..density..,
       color = ~ lev,
       fill = ~ lev
     )
@@ -54,16 +65,19 @@ mcmc_rhat_hist <- function(rhat, ..., binwidth = NULL) {
 #' @param size An optional value to override \code{\link[ggplot2]{geom_point}}'s
 #'   default size.
 #'
-mcmc_rhat_dot <- function(rhat, ..., size = NULL) {
+mcmc_rhat_dots <- function(rhat, ..., size = NULL) {
   stopifnot(length(rhat) > 1)
 
+  # factor rhat by parameter instead of value
   frhat <- if (!is.null(names(rhat))) {
     factor(rhat, labels = names(rhat))
   } else {
     factor(rhat)
   }
   data <- data.frame(y = rhat, x = frhat)
-  graph <- ggplot(data, aes_(x = ~ x, y = ~ y))
+  graph <- ggplot(data, aes_(x = ~ x, y = ~ y,
+                             color = ~factor_rhat(rhat),
+                             fill = ~factor_rhat(rhat)))
 
   if (any(rhat > 1.05))
     graph <- graph + geom_hline(
@@ -81,8 +95,8 @@ mcmc_rhat_dot <- function(rhat, ..., size = NULL) {
     )
 
   .rhat_dots <- function(size = NULL) {
-    args <- list(color = get_color("mh"),
-                 fill = get_color("m"),
+    args <- list(#color = get_color("mh"),
+                 # fill = get_color("m"),
                  shape = 21,
                  na.rm = TRUE)
     do.call("geom_point", c(args, size = size))
@@ -90,11 +104,10 @@ mcmc_rhat_dot <- function(rhat, ..., size = NULL) {
 
   graph +
     geom_segment(
-      aes_(xend = ~x, yend = ~1),
-      color = get_color("l"),
+      aes_(xend = ~x, yend = ~1, color = ~factor_rhat(rhat)),
       na.rm = TRUE
     ) +
-    geom_hline(yintercept = 1) +
+    geom_hline(yintercept = 1, color = get_color("dh")) +
     .rhat_dots(size) +
     labs(x = NULL, y = bquote(hat(R))) +
     scale_fill_rhat() +
