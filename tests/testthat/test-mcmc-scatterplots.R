67c5392092963067eb49571194ad668a27839134
jgabry <jgabry@gmail.com>
2016-07-25 10:38:34 -0400

    test mcmc_scatter

--- tests/testthat/test-mcmc-scatterplots.R
+++ tests/testthat/test-mcmc-scatterplots.R
@@ -1,7 +1,25 @@
 library(bayesplot)
-context("MCMC: pairs")
+context("MCMC: scatterplots")
 
 source("data-for-mcmc-tests.R")
+
+
+test_that("mcmc_scatter returns a ggplot object", {
+  expect_gg(mcmc_scatter(arr, pars = c("beta[1]", "beta[2]")))
+  expect_gg(mcmc_scatter(arr1chain, regex_pars = "beta"))
+  expect_gg(mcmc_scatter(mat, pars = c("sigma", "(Intercept)")))
+  expect_gg(mcmc_scatter(dframe, regex_pars = "x:[2,4]"))
+  expect_gg(mcmc_scatter(dframe_multiple_chains,
+                         pars = c("sigma", "(Intercept)")))
+})
+
+test_that("mcmc_scatter throws error if only 1 parameter", {
+  expect_error(mcmc_scatter(arr, pars = "sigma"), "exactly 2 parameters")
+  expect_error(mcmc_scatter(arr1), "exactly 2 parameters")
+  expect_error(mcmc_scatter(mat1), "exactly 2 parameters")
+  expect_error(mcmc_scatter(dframe1), "exactly 2 parameters")
+  expect_error(mcmc_scatter(chainlist1), "exactly 2 parameters")
+})
 
 test_that("mcmc_pairs returns a gg object", {
   expect_message(mcmc_pairs(arr, pars = c("(Intercept)", "sigma")),
@@ -23,7 +41,3 @@ test_that("mcmc_pairs throws error if only 1 parameter", {
   expect_error(mcmc_pairs(dframe1), "requires multiple parameters")
   expect_error(mcmc_pairs(chainlist1), "requires multiple parameters")
 })
-
-
-
-
