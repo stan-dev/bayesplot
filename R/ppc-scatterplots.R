ba949d3eaa0174ebe47816d6e78dd40e04bab3e3
jgabry <jgabry@gmail.com>
2016-08-16 16:04:12 -0400

    swap axes in ppc scatterplots
    closes #19

--- R/ppc-scatterplots.R
+++ R/ppc-scatterplots.R
@@ -61,9 +61,9 @@ ppc_scatter <- function(y, yrep, ..., size = 1.5, alpha = 0.8) {
 
   graph <- ppc_scatter_plotter(
     data = data.frame(melt_yrep(yrep), y = rep(y, each = nrow(yrep))),
-    mapping = aes_(x = ~ y, y = ~ value),
-    x_lab = y_label(),
-    y_lab = yrep_label(),
+    mapping = aes_(x = ~ value, y = ~ y),
+    y_lab = y_label(),
+    x_lab = yrep_label(),
     alpha = alpha,
     size = size
   )
@@ -79,9 +79,9 @@ ppc_scatter_avg <- function(y, yrep, ..., size = 2.5, alpha = 0.8) {
 
   ppc_scatter_plotter(
     data = data.frame(y, avg_y_rep = colMeans(yrep)),
-    mapping = aes_(x = ~ y, y = ~ avg_y_rep),
-    x_lab = y_label(),
-    y_lab = yrep_avg_label(),
+    mapping = aes_(x = ~ avg_y_rep, y = ~ y),
+    y_lab = y_label(),
+    x_lab = yrep_avg_label(),
     alpha = alpha,
     size = size
   )
@@ -99,9 +99,9 @@ ppc_scatter_avg_grouped <- function(y, yrep, group, ...,
 
   graph <- ppc_scatter_plotter(
     data = data.frame(group, y, avg_yrep = colMeans(yrep)),
-    mapping = aes_(x = ~ y, y = ~ avg_yrep),
-    x_lab = y_label(),
-    y_lab = yrep_avg_label(),
+    mapping = aes_(x = ~ avg_yrep, y = ~ y),
+    y_lab = y_label(),
+    x_lab = yrep_avg_label(),
     alpha = alpha,
     size = size
   )
