30dad0b71470bffef0db08e3bc0c5ce5f7d4b499
jgabry <jgabry@gmail.com>
2016-10-31 16:22:38 -0400

    improve checks in validate_nuts_data_frame

--- tests/testthat/test-mcmc-nuts.R
+++ tests/testthat/test-mcmc-nuts.R
@@ -47,3 +47,32 @@ test_that("mcmc_nuts_energy returns a ggplot object", {
 test_that("mcmc_nuts_energy throws correct warnings", {
   expect_warning(mcmc_nuts_energy(np, chain = 1), "ignored: chain")
 })
+
+
+test_that("validate_nuts_data_frame throws errors", {
+  expect_error(
+    validate_nuts_data_frame(list(Iteration = 1, Chain = 1)),
+    "NUTS parameters should be in a data frame"
+  )
+  expect_error(
+    validate_nuts_data_frame(data.frame(Iteration = 1, apple = 2)),
+    "NUTS parameter data frame must have columns: Iteration, Parameter, Value, Chain"
+  )
+  expect_error(
+    validate_nuts_data_frame(np, as.matrix(lp)),
+    "lp should be in a data frame"
+  )
+
+  lp2 <- lp
+  colnames(lp2)[3] <- "Chains"
+  expect_error(
+    validate_nuts_data_frame(np, lp2),
+    "lp data frame must have columns: Iteration, Value, Chain"
+  )
+
+  lp2 <- subset(lp, Chain %in% 1:2)
+  expect_error(
+    validate_nuts_data_frame(np, lp2),
+    "Number of chains"
+  )
+})
