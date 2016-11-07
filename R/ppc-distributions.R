0bda00570b7891378d2b8320c981ed10af193139
jgabry <jgabry@gmail.com>
2016-11-07 12:38:50 -0500

    update traces doc

--- R/ppc-distributions.R
+++ R/ppc-distributions.R
@@ -278,6 +278,7 @@ ppc_dens_overlay <- function(y, yrep, ...,
       aes_(color = "y"),
       geom = "line",
       position = "identity",
+      lineend = "round",
       size = 1,
       trim = trim
     ) +
