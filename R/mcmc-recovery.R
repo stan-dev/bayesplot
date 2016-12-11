9b3956c4d62ec4890ce965a5b42a22f54a3afe06
jgabry <jgabry@gmail.com>
2016-12-11 01:41:38 -0500

    add batch argument

--- R/mcmc-recovery.R
+++ R/mcmc-recovery.R
@@ -1,4 +1,4 @@
-#' Compare MCMC estimates to true parameter values
+#' Compare MCMC estimates to "true" parameter values
 #'
 #' See the \strong{Plot Descriptions} section, below, for details.
 #'
@@ -10,12 +10,32 @@
 #'   \code{x}. There should be one value in \code{true} for each parameter
 #'   included in \code{x} and the order of the parameters in \code{true} should
 #'   be the same as the order of the parameters in \code{x}.
+#' @param batch Optionally, a vector (numeric, character, integer, factor) used
+#'   to split the parameters into batches. If specified \code{batch} must have
+#'   the same length as \code{true} and be in the same order as \code{true}.
+#'   Parameters in the same batch will be grouped together in the same facet in
+#'   the plot (see the \strong{Examples} section, below). The default is to
+#'   group all parameters together into a single batch. Changing the default is
+#'   most useful when parameters are on very different scales, in which case
+#'   \code{batch} can be used to group them into batches for within which it
+#'   makes sense to use the same \eqn{y}-axis.
+#' @param facet_args Arguments (other than \code{facets}) passed to
+#'   \code{\link[ggplot2]{facet_wrap}} to control faceting.
 #' @param ... Currently unused.
 #' @param prob The probability mass to include in the interval. The
 #'   default is \code{0.5} (50\% interval).
 #' @param point_est The point estimate to show. Either \code{"median"} (the
 #'   default), \code{"mean"}, or \code{"none"}.
-#' @param size Passed to \code{\link[ggplot2]{geom_point}}.
+#'
+#' @template return-ggplot
+#'
+#' @section Plot Descriptions:
+#' \describe{
+#'   \item{\code{mcmc_recover_intervals}}{
+#'    Plots comparing "true" parameter values to estimates (intervals and point
+#'    estimates) computed from MCMC draws.
+#'   }
+#' }
 #'
 #' @examples
 #' \dontrun{
@@ -26,32 +46,59 @@
 #' fit <- stan_glm(y ~ X)
 #' draws <- as.matrix(fit)
 #' print(colnames(draws))
-#' mcmc_recover(draws, true, prob = )
+#' true <- c(alpha, beta, sigma)
+#' mcmc_recover_intervals(draws, true)
+#'
+#' # put the coefficients on X into the same batch
+#' mcmc_recover_intervals(draws, true, batch = c(1, 2, 2, 1))
+#' # equivalent
+#' mcmc_recover_intervals(draws, true, batch = grepl("X", colnames(draws)))
+#' # same but stacked vertically
+#' mcmc_recover_intervals(draws, true,
+#'                        batch = grepl("X", colnames(draws)),
+#'                        facet_args = list(ncol = 1))
+#'
+#' # each parameter in its own facet
+#' mcmc_recover_intervals(draws, true, batch = 1:4)
+#' # same but in a different order
+#' mcmc_recover_intervals(draws, true, batch = c(1, 3, 4, 2))
 #' }
 #'
 NULL
 
 #' @rdname MCMC-recovery
 #' @export
-mcmc_recover <-
+mcmc_recover_intervals <-
   function(x,
            true,
+           batch = rep(1, length(true)),
            facet_args = list(),
            ...,
            prob = 0.5,
            prob_outer = 0.9,
            point_est = c("median", "mean")) {
-    x <- merge_chains(prepare_mcmc_array(x))
-    stopifnot(prob_outer >= prob)
+    check_ignored_arguments(...)
+    point_est <- match.arg(point_est)
+    stopifnot(is.numeric(true), length(batch) == length(true))
+    stopifnot(prob_outer >= prob, prob > 0, prob_outer <= 1)
     alpha1 <- (1 - prob) / 2
     alpha2 <- (1 - prob_outer) / 2
     probs <- sort(c(alpha1, 1 - alpha1, alpha2, 1 - alpha2))
+
+    x <- merge_chains(prepare_mcmc_array(x))
     intervals <- t(apply(x, 2, quantile, probs = probs))
     colnames(intervals) <- c("ll", "l", "u", "uu")
-    point <- apply(x, 2, match.arg(point_est))
-    plot_data <- data.frame(Parameter = rownames(intervals),
-                            intervals, point, true)
-    facet_args[["facets"]] <- ~ Parameter
+    point <- apply(x, 2, point_est)
+
+    all_separate <- length(unique(batch)) == length(true)
+    plot_data <- data.frame(
+      Parameter = rownames(intervals),
+      Batch = if (!all_separate) batch else rownames(intervals),
+      True = true,
+      Point = point,
+      intervals
+    )
+    facet_args[["facets"]] <- ~ Batch
     if (is.null(facet_args[["strip.position"]]))
       facet_args[["strip.position"]] <- "top"
     if (is.null(facet_args[["scales"]]))
@@ -59,43 +106,52 @@ mcmc_recover <-
 
     plot_caption <- paste0("Showing ", round(prob * 100, 1), "% and ",
                            round(prob_outer * 100, 1), "% intervals")
-    ggplot(plot_data, aes_(x = 0, xend = 0)) +
+    graph <- ggplot(plot_data, aes_(x = ~ Parameter, xend = ~ Parameter)) +
       geom_segment(
-        aes_(y = ~ ll, yend = ~ uu),
-        color = get_color("lh"),
-        lineend = "round"
+        aes_(y = ~ ll, yend = ~ uu, color = "Estimated"),
+        lineend = "round",
+        show.legend = FALSE
       ) +
       geom_segment(
-        aes_(y = ~ l, yend = ~ u),
-        color = get_color("l"),
+        aes_(y = ~ l, yend = ~ u, color = "Estimated"),
         size = 2,
-        lineend = "round"
+        lineend = "round",
+        show.legend = FALSE
       ) +
       geom_point(
-        aes_(y = ~ point,
-             shape = "Estimated",
-             color = "Estimated",
-             fill = "Estimated"),
+        aes_(y = ~ Point, shape = "Estimated",
+             color = "Estimated", fill = "Estimated"),
         size = 4
       ) +
       geom_point(
-        aes_(y = ~ true,
-             shape = "True",
-             color = "True",
-             fill = "True"),
+        aes_(y = ~ True, shape = "True",
+             color = "True", fill = "True"),
         size = 4
       ) +
-      scale_color_manual("", values = c(Estimated = get_color("l"), True = get_color("dh"))) +
-      scale_fill_manual("", values = c(Estimated = get_color("m"), True = get_color("dh"))) +
-      scale_shape_manual("", values = c(Estimated = 21, True = 24)) +
-      scale_x_continuous(limits = c(-0.5, 1)) +
+      scale_color_manual(
+        name = "",
+        values = c(Estimated = get_color("d"), True = get_color("dh"))
+      ) +
+      scale_fill_manual(
+        name = "",
+        values = c(Estimated = get_color("d"), True = get_color("l"))
+      ) +
+      scale_shape_manual(
+        name = "",
+        values = c(Estimated = 21, True = 24)
+      ) +
       do.call("facet_wrap", facet_args) +
-      labs(y = "Value", x = "Parameter", caption = plot_caption) +
+      labs(y = "Value", x = "Parameter", subtitle = plot_caption) +
       theme_default() +
-      theme(axis.line.x = element_blank(),
-            plot.caption = element_text(hjust = 0)) +
-      xaxis_ticks(FALSE) +
-      xaxis_text(FALSE) +
+      theme(plot.caption = element_text(hjust = 0)) +
       xaxis_title(FALSE) +
       yaxis_title(FALSE)
+
+    if (!all_separate)
+      return(graph + xaxis_text(face = "bold") + facet_text(FALSE))
+
+    graph +
+      theme(axis.line.x = element_blank()) +
+      xaxis_ticks(FALSE) +
+      xaxis_text(FALSE)
   }
