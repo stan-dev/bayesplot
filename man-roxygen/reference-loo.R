33ee74abdac87ccb2184ec5185e48df9b3e76584
jgabry <jgabry@gmail.com>
2017-05-09 12:47:53 -0400

    Update loo reference

--- man-roxygen/reference-loo.R
+++ man-roxygen/reference-loo.R
@@ -1,5 +1,5 @@
-#' @references Vehtari, A., Gelman, A., and Gabry, J. (2016). Practical
+#' @references Vehtari, A., Gelman, A., and Gabry, J. (2017). Practical
 #'   Bayesian model evaluation using leave-one-out cross-validation and WAIC.
-#'   \emph{Statistics and Computing}. Advance online publication.
+#'   \emph{Statistics and Computing}. 27(5), 1413--1432.
 #'   doi:10.1007/s11222-016-9696-4. arXiv preprint:
 #'   \url{http://arxiv.org/abs/1507.04544/}
