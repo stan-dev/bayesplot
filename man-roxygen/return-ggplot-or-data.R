85e2ddcc320da1273109c3b71930830bf240ed70
jgabry <jgabry@gmail.com>
2018-04-08 15:57:14 -0400

    Fix typo in roxygen template
    [ci skip]

--- man-roxygen/return-ggplot-or-data.R
+++ man-roxygen/return-ggplot-or-data.R
@@ -1,3 +1,3 @@
 #' @return A ggplot object that can be further customized using the
 #'   \pkg{ggplot2} package. The \code{_data} functions return the data that
-#'   would have be drawn by the plotting function.
+#'   would have been drawn by the plotting function.
