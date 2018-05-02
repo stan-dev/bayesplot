e1f8a2c8e0f780818d50ca7d81cf134578bf61f3
Malcolm Barrett <malcolmbarrett@gmail.com>
2018-05-02 08:24:50 -1000

    make startup message a little clearer

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onAttach <- function(...) {
   ver <- utils::packageVersion("bayesplot")
   packageStartupMessage("This is bayesplot version ", ver)
-  packageStartupMessage("- Plotting theme set to bayesplot::theme_default(). Change it")
+  packageStartupMessage("- bayesplot theme set to bayesplot::theme_default(). Change it")
   packageStartupMessage("  with ggplot2::theme_set() or bayesplot::bayesplot_theme_set()")
   packageStartupMessage("- Online documentation at mc-stan.org/bayesplot")
 }
