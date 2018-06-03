2f2b4a1015fb337adc47e257ba0f0b21bcefd871
jgabry <jgabry@gmail.com>
2018-06-03 16:27:42 -0400

    Reference Ari's post in mcmc_parcoord doc
    Add reference to post by @ahartikainen to the Stan Forums with the example of the parallel coordinates plot

--- R/mcmc-parcoord.R
+++ R/mcmc-parcoord.R
@@ -49,6 +49,9 @@
 #' }
 #'
 #' @template reference-vis-paper
+#' @references Hartikainen, A. (2017, Aug 23). Concentration of divergences
+#' [Msg 21]. Message posted to The Stan Forums:
+#' \url{http://discourse.mc-stan.org/t/concentration-of-divergences/1590/21}.
 #'
 #' @examples
 #' color_scheme_set("pink")
