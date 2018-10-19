b13e845e21acd7d49fad23c187dc4b166b9539a4
TJ Mahr <mahr@wisc.edu>
2018-10-19 09:33:34 -0500

    alternative testing route for interactive sessions

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,3 +3,9 @@ library(bayesplot)
 
 Sys.unsetenv("R_TESTS")
 test_check("bayesplot")
+
+# Alternative interactive tester that doesn't bail after 24 failures
+# pr <- testthat::ProgressReporter$new()
+# pr$max_fail = 1000
+# devtools::test(reporter = pr)
+
