79c4d5acef3c5c762369c203135d5300b2212f13
jgabry <jgabry@gmail.com>
2019-05-17 18:21:11 -0400

    edits to sbc_hist to fit in with bayesplot conventions

--- R/sbc.R
+++ R/sbc.R
@@ -1,106 +1,175 @@
-klUniform <- function(v, numPriorDraws, samplesPerPrior) {
-  # D_{KL}(v || uniform)
-  # https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
-  expectedPr <- 1.0/samplesPerPrior
-  observedPr <- table(v) / numPriorDraws
-  sum(observedPr * log(observedPr/expectedPr))
-}
-
-#' Histograms for Simulation Based Calibration
+#' Diagnostics for Simulation Based Calibration
 #'
-#' @param ranks A list of sampling realizations.
-#' @param thin An integer vector of length one indicating the thinning interval
-#' when plotting
-#' @param perBin Number of histogram entries to combine into a single bar.
-#' @param worst If NA, plots all parameters. Otherwise how many parameters to show.
-#' Parameters are ordered by the degree of non-uniformity.
-#' @param alpha Uncertainty interval probability for a false positive (alpha level).
-#' @param hideAxes Whether to hide the plot axes.
+#' @export
+#' @param ranks A list of matrices. See **Details**.
+#' @param thin An integer indicating the thinning interval to use when plotting
+#'   so that the histograms consist of (close to) independent realizations. Set
+#'   the `thin` argument such that the resulting number of draws approximately
+#'   matches the effective sample size.
+#' @param per_bin An integer indicating the number of histogram entries to
+#'   combine into a single bar. For best results, one plus the number of draws
+#'   from the posterior should be evenly divisible by the number of histogram
+#'   bins after thinning. For example, if there are `511` posterior draws (per
+#'   matrix in `ranks`) and `thin=4`, then after thinning there will be `128`
+#'   draws. If `per_bin=4`, then `128/4=32` histogram bars will be drawn.
+#' @param worst How many parameters to show, or `NA` to plot all parameters. If
+#'   `worst` is not `NA` then parameters are ordered by the degree of
+#'   non-uniformity so, for example, `worst = 10` means to plot only the `10`
+#'   worst parameters.
+#' @param prob The size of the interval plotted to show the expected behavior
+#'   under uniformity. The default is `prob=0.99`.
+#' @template args-facet_args
 #'
-#' Each list element of \code{ranks} should be a matrix of rank
-#' comparison results (encoded as 0 or 1) associated with a single
-#' draw from the prior distribution. Each draw from the posterior is
-#' in the row and parameters are in columns. The matrix should have
-#' column names to correctly label the parameters.
+#' @details
+#' Each element of the list `ranks` should be a matrix of rank comparison
+#' results (encoded as `0` or `1`) associated with a single draw from the prior
+#' distribution. (These are not actually "ranks" but can be used afterwards to
+#' reconstruct (thinned) ranks.) The columns of each matrix are model parameters
+#' and the rows are posterior draws from the model fit to the data generated
+#' from the corresponding prior draw. An element is `0` or `1` depending on
+#' whether the posterior draw is greater or less than the corresponding "true"
+#' realization (from the prior). The matrices should have column names to
+#' correctly label the parameters.
 #'
-#' So that the histograms consist of independent realizations,
-#' draws from the posterior should be thinned to remove
-#' autocorrelation. Set \code{thin} such that the number of
-#' draws approximately matches the effective sample size.
+#' @section Plot Descriptions:
+#' \describe{
+#'   \item{`sbc_hist()`}{
+#'   SBC histogram from Talts et al. (2018). A separate plot (facet) is created
+#'   for each parameter.
+#'   }
+#' }
 #'
-#' For best results, one plus the number of draws from the posterior
-#' should be evenly divisible by the number of histogram bins after
-#' thinning. For example, 511 draws after thinning results in 128
-#' draws. If perBin is set to 4 then 32 histogram bars are drawn.
-#' 
 #' @template return-ggplot
 #'
 #' @references
-#'   Talts, S., Betancourt, M., Simpson, D., Vehtari, A., and Gelman, A. (2018).
-#'   Validating Bayesian Inference Algorithms with Simulation-Based Calibration.
-#'   arXiv preprint arXiv:1804.06788. \url{https://arxiv.org/abs/1804.06788}
-#' @seealso
-#' \link[rstan]{sbc}
+#' Talts, S., Betancourt, M., Simpson, D., Vehtari, A., and Gelman, A. (2018).
+#' Validating Bayesian Inference Algorithms with Simulation-Based Calibration.
+#' arXiv preprint arXiv:1804.06788. \url{https://arxiv.org/abs/1804.06788}
+#'
+#' @seealso [rstan::sbc()]
+#'
 #' @examples
-#' pars <- paste0('parameter',1:2)
-#' samplesPerPrior <- 511
+#' # create some fake inputs to use for sbc_hist()
+#' set.seed(19)
+#' pars <- paste0("beta[", 1:4, "]")
+#' samples_per_prior <- 511
+#' n_replications <- 500
 #' ranks <- list()
-#' for (px in 1:500) {
-#'   r1 <- matrix(0, nrow=samplesPerPrior, ncol=length(pars),
-#'          dimnames=list(NULL, pars))
+#' for (n in 1:n_replications) {
+#'   r1 <- matrix(0, nrow=samples_per_prior, ncol=length(pars),
+#'                dimnames=list(NULL, pars))
 #'   for (p1 in 1:length(pars)) {
-#'     r1[sample.int(samplesPerPrior,
-#'                   floor(runif(1, 0, samplesPerPrior))), p1] <- 1
+#'     r1[sample.int(samples_per_prior, floor(runif(1, 0, samples_per_prior))), p1] <- 1
 #'   }
-#'   ranks[[px]] <- r1
+#'   ranks[[n]] <- r1
 #' }
+#'
+#' color_scheme_set("purple")
 #' sbc_hist(ranks)
-#' @export
+#'
+sbc_hist <- function(ranks, ...,
+                     thin = 4, per_bin = 4, worst = 16, prob = 0.99,
+                     facet_args = list()) {
+  check_ignored_arguments(...)
+  stopifnot(is.list(ranks), all(sapply(ranks, is.matrix)))
+  rows <- sapply(ranks, nrow)
+  cols <- sapply(ranks, ncol)
+  if (any(rows != rows[1]) || any(cols != cols[1])) {
+    stop("Not all matrices in 'ranks' have the same dimensions.")
+  }
 
-sbc_hist <- function(ranks, thin = 4, perBin=4, worst=16, ...,
-                     alpha = 0.01, hideAxes=TRUE) {
-  numPriorDraws <- length(ranks)
+  num_prior_draws <- length(ranks)
   thinner <- seq(from = 1, to = nrow(ranks[[1]]), by = thin)
-  samplesPerPrior <- length(thinner)
+  samples_per_prior <- length(thinner)
   u <- t(sapply(ranks, FUN = function(r) 1 + colSums(r[thinner, , drop = FALSE])))
   if (ncol(ranks[[1]]) == 1) {
     u <- t(u)
     dimnames(u) <- list(NULL, colnames(ranks[[1]]))
   }
-  
+
   if (!is.na(worst)) {
-    kl <- apply(u, 2, function(v) klUniform(v, numPriorDraws, samplesPerPrior))
-    filter <- order(-kl)[1:min(worst,ncol(u))]
-#    print(filter)
-    u <- u[, filter, drop=FALSE ]
+    # order starting with worst (least uniform)
+    kl <- apply(u, 2, function(v) kl_uniform(v, num_prior_draws, samples_per_prior))
+    filter <- order(-kl)[1:min(worst, ncol(u))]
+    u <- u[, filter, drop = FALSE]
   }
-  
-  parameter <- ordered(rep(colnames(u), each = nrow(u)),
-                       levels=colnames(u))
-  d <- data.frame(u = c(u), parameter)
-  if (samplesPerPrior %% perBin != 0) {
-    warning(paste("perBin (", perBin, ") does not evenly divide the",
-      "number of samples per prior (",samplesPerPrior,")"))
+
+  pars <- parameter_names(u)
+  data <- data.frame(
+    Parameter = ordered(rep(pars, each = nrow(u)), levels = pars),
+    u = c(u)
+  )
+
+  num_bins <- samples_per_prior / per_bin
+  if (samples_per_prior %% per_bin != 0) {
+    warning(paste0("per_bin (", per_bin, ") does not evenly divide the",
+                   "number of samples per prior (", samples_per_prior ,")."))
   }
-  numBins <- samplesPerPrior/perBin
-  CI <- qbinom(c(alpha/2,0.5,1-alpha/2), numPriorDraws, numBins^-1) + c(-.5,0,.5)
-  offset <- perBin*2
-  pl <- ggplot(d, aes(x = u)) + 
-    geom_polygon(data=data.frame(x=c(-offset,0,-offset,samplesPerPrior + offset,
-      samplesPerPrior, samplesPerPrior + offset,-offset),
-      y=c(CI[1],CI[2],CI[3],CI[3],CI[2],CI[1],CI[1])),
-      aes(x=x,y=y),fill="grey45",color="grey25",alpha=0.5) +
-    geom_histogram(bins=numBins, na.rm=TRUE) +
-#    xlim(1,samplesPerPrior) +
-    # https://github.com/tidyverse/ggplot2/issues/3332
-    facet_wrap("parameter") +
-    geom_hline(yintercept=CI[1], color='green', linetype="dotted", alpha=.5) +
-  geom_hline(yintercept=CI[3], color='green', linetype="dotted", alpha=.5)
-  if (hideAxes) {
-    pl <- pl + theme(axis.text.x=element_blank(),
-      axis.text.y=element_blank(),axis.ticks=element_blank(),
-      axis.title.x=element_blank(),
-      axis.title.y=element_blank())
-    }
-  pl
+
+  # data for polygon showing expected behavior under uniformity
+  alpha <- 1 - prob
+  CI <- qbinom(
+    p = c(alpha / 2, 0.5, 1 - alpha / 2),
+    size = num_prior_draws,
+    prob = 1 / num_bins
+  )
+  CI <- CI + c(-.5, 0, .5)
+  offset <- 2 * per_bin
+  polygon_data <- data.frame(
+    x= c(-offset, 0, -offset, samples_per_prior + offset,
+         samples_per_prior, samples_per_prior + offset, -offset),
+    y = c(CI[1], CI[2], CI[3], CI[3], CI[2], CI[1], CI[1])
+  )
+
+  graph <- ggplot(data, aes_(x = ~ u)) +
+    geom_polygon(
+      aes_(x = ~ x, y = ~ y),
+      data = polygon_data,
+      fill = "lightgray",
+      color = NA,
+      alpha = 1
+    ) +
+    geom_segment(
+      x = 0,
+      xend = samples_per_prior,
+      y = CI[2],
+      yend = CI[2],
+      color = "darkgray",
+      alpha = 0.5,
+      size = 0.1
+    ) +
+    geom_histogram(
+      bins = num_bins,
+      fill = get_color("mid"),
+      color = get_color("mid_highlight"),
+      size = .25,
+      na.rm = TRUE
+    ) +
+    xlab("Rank statistic") +
+    coord_cartesian(
+      # xlim = c(-per_bin, samples_per_prior + per_bin),
+      expand = FALSE
+    )
+
+  facet_args[["facets"]] <- ~ Parameter
+  graph +
+    do.call("facet_wrap", facet_args) +
+    bayesplot_theme_get() +
+    xaxis_text(FALSE) +
+    yaxis_text(FALSE) +
+    xaxis_title(TRUE) +
+    yaxis_title(FALSE) +
+    xaxis_ticks(FALSE) +
+    yaxis_ticks(FALSE)
+}
+
+
+
+# internal ----------------------------------------------------------------
+kl_uniform <- function(v, num_prior_draws, samples_per_prior) {
+  # D_{KL}(v || uniform)
+  # https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+  expected_pr <- 1.0/samples_per_prior
+  observed_pr <- table(v) / num_prior_draws
+  sum(observed_pr * log(observed_pr/expected_pr))
 }
