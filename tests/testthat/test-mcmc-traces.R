04b0b2b5debfa5c87d67050cf7b67824b00ddc9d
jgabry <jgabry@gmail.com>
2020-06-18 19:44:09 -0400

    add skip_if_not_installed("vdiffr") a few more times

--- tests/testthat/test-mcmc-traces.R
+++ tests/testthat/test-mcmc-traces.R
@@ -120,6 +120,7 @@ test_that("mcmc_trace renders correctly", {
 
 test_that("mcmc_rank_overlay renders correctly", {
   testthat::skip_on_cran()
+  testthat::skip_if_not_installed("vdiffr")
 
   p_base <- mcmc_rank_overlay(vdiff_dframe_chains, pars = c("V1", "V2"))
   p_base_ref <- mcmc_rank_overlay(
@@ -148,6 +149,7 @@ test_that("mcmc_rank_overlay renders correctly", {
 
 test_that("mcmc_rank_hist renders correctly", {
   testthat::skip_on_cran()
+  testthat::skip_if_not_installed("vdiffr")
 
   p_base <- mcmc_rank_hist(vdiff_dframe_chains, pars = c("V1", "V2"))
   p_base_ref <- mcmc_rank_hist(
@@ -179,6 +181,7 @@ test_that("mcmc_rank_hist renders correctly", {
 
 test_that("mcmc_trace_highlight renders correctly", {
   testthat::skip_on_cran()
+  testthat::skip_if_not_installed("vdiffr")
 
   p_base <- mcmc_trace_highlight(
     vdiff_dframe_chains,
@@ -206,6 +209,7 @@ test_that("mcmc_trace_highlight renders correctly", {
 
 test_that("mcmc_trace with 'np' renders correctly", {
   testthat::skip_on_cran()
+  testthat::skip_if_not_installed("vdiffr")
 
   p_base <- mcmc_trace(
     vdiff_dframe_chains,
