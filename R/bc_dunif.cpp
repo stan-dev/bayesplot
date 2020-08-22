25a2199664068cef4618477a4bc518c8018cc53a
Eduardo Coronado <e.coronado@duke.edu>
2020-08-22 15:54:23 -0400

    updated cpp code

--- R/bc_dunif.cpp
+++ R/bc_dunif.cpp
@@ -1,3 +1,4 @@
+#include <Rcpp.h>
 #include <RcppEigen.h>
 #include <cmath>
 
@@ -6,6 +7,7 @@
 using Eigen::Map;                       // 'maps' rather than copies
 using Eigen::VectorXd;                  // variable size vector, double precision
 using Eigen::RowVectorXd;               // double precision row vector
+using Rcpp::NumericVector;
 
 
 // Bias correction function (Chen 1999)
@@ -23,25 +25,27 @@ VectorXd vec_dbeta(Map<VectorXd> x, double a, double b){
   for (int j = 0; j < n; j++){
     d(j) = R::dbeta(x(j), a, b, 0);  // scalar output 
   }
-  
   return d;
 }
 
 // Piecewise kernel value estimation (Chen 1999)
- double bc_kde_calc(double xs, double b,  Map<VectorXd> pvals){
+ double bc_kde_calc(double xs, double b,  NumericVector pvals){
   
   // Memory allocation
   double d;
-  VectorXd tmp;
-   
+  NumericVector out;
+
   if ((xs >= 2*b) && (xs <= (1 - 2*b))){
-    tmp = vec_dbeta(pvals, xs/b, (1 - xs)/b); // helper custom fuction
+    out = Rcpp::dbeta(pvals, xs/b, (1 - xs)/b, 0.0);
+    Map<VectorXd>  tmp(Rcpp::as< Map<VectorXd> >(out));
     d = tmp.sum() / tmp.size();               // vector mean
   } else if ((xs >= 0) && (xs < 2*b)) {
-    tmp = vec_dbeta(pvals, rho(xs, b), (1 - xs)/b);
+    out = Rcpp::dbeta(pvals, rho(xs, b), (1 - xs)/b, 0.0);
+    Map<VectorXd>  tmp(Rcpp::as< Map<VectorXd> >(out));
     d = tmp.sum() / tmp.size();
   } else if ((xs > (1 - 2*b)) && (xs <= 1)) {
-    tmp = vec_dbeta(pvals, xs/b, rho(1 - xs, b));
+    out = Rcpp::dbeta(pvals, xs/b, rho(1 - xs, b), 0.0);
+    Map<VectorXd> tmp(Rcpp::as< Map<VectorXd> >(out));
     d = tmp.sum() / tmp.size();
   } else {
     d = 0.0;
@@ -50,9 +54,8 @@ VectorXd vec_dbeta(Map<VectorXd> x, double a, double b){
 }
 
 
-
 // [[Rcpp::export]]
-RowVectorXd bc_dunif(Map<VectorXd> xs, Map<VectorXd> pvals, 
+RowVectorXd bc_dunif(Map<VectorXd> xs, NumericVector pvals, 
                      double b, double xmax = 1.0){
   // Function based on biased-corrected (modified) beta kernel 
   //Chen, Song Xi. "Beta kernel estimators for density functions." 
