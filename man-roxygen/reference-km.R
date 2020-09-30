3836da1216c3423767fb8f31f194bd9b0d9eb5b0
fweber144 <fweber144@protonmail.com>
2020-09-30 16:07:18 +0200

    For Kaplan and Meier (1958), follow the citation style from "reference-loo.R".

--- man-roxygen/reference-km.R
+++ man-roxygen/reference-km.R
@@ -1,3 +1,4 @@
-#' @references Kaplan, E. L. and Meier, P. (1958), Nonparametric estimation
-#'   from incomplete observations. *J. Am. Stat. Assoc.*, 53(282): 457-481.
+#' @references Kaplan, E. L. and Meier, P. (1958). Nonparametric estimation
+#'   from incomplete observations.
+#'   *Journal of the American Statistical Association*. 53(282), 457--481.
 #'   doi:10.1080/01621459.1958.10501452.
