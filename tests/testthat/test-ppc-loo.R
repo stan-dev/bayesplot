9599ba275fbd52c808bee6bbaf1e2ebc9318cd33
fweber144 <fweber144@protonmail.com>
2020-10-20 12:55:51 +0200

    Move two unit tests for ppc_loo_pit_qq() to a more appropriate place.

--- tests/testthat/test-ppc-loo.R
+++ tests/testthat/test-ppc-loo.R
@@ -28,13 +28,13 @@ test_that("ppc_loo_pit gives deprecation warning but still works", {
 
 test_that("ppc_loo_pit_overlay returns ggplot object", {
   expect_gg(p1 <- ppc_loo_pit_overlay(y, yrep, lw, samples = 25))
-  expect_gg(p2 <- ppc_loo_pit_qq(y, yrep, lw, compare = "normal"))
-  expect_equal(p2$labels$x, "Normal")
 })
 
 test_that("ppc_loo_pit_qq returns ggplot object", {
   expect_gg(p1 <- ppc_loo_pit_qq(y, yrep, lw))
   expect_equal(p1$labels$x, "Uniform")
+  expect_gg(p2 <- ppc_loo_pit_qq(y, yrep, lw, compare = "normal"))
+  expect_equal(p2$labels$x, "Normal")
 })
 
 test_that("ppc_loo_pit functions work when pit specified instead of y,yrep,lw", {
