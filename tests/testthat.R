93a603d2117a357a3d45ee8cf971c0bc18ce0bb5
jgabry <jgabry@gmail.com>
2020-12-17 12:59:18 -0700

    see if check.environment = FALSE fixes the failing tests on r-devel

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,12 +8,3 @@ test_check("bayesplot")
 # pr <- testthat::ProgressReporter$new()
 # pr$max_fail = 1000
 # devtools::test(reporter = pr)
-
-# pass check.environment=FALSE to all.equal to avoid issue due to
-# changing behavior of all.equal:
-# https://stat.ethz.ch/pipermail/r-devel/2020-December/080172.html
-expect_equal <- function(...) {
-  testthat::expect_equal(..., check.environment=FALSE)
-}
-
-
