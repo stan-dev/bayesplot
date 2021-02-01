0f72378cccdcb99ae49d48d1a1a8d2f34a08790e
fweber144 <fweber144@protonmail.com>
2021-02-01 16:12:36 +0100

    Moved unit tests for "ppc-censoring" to their own file.

---
+++ tests/testthat/test-ppc-censoring.R
@@ -0,0 +1,27 @@
+library(bayesplot)
+context("PPC: censoring")
+
+source(test_path("data-for-ppc-tests.R"))
+
+test_that("ppc_km_overlay returns a ggplot object", {
+  expect_gg(ppc_km_overlay(y, yrep, status_y = status_y, size = 0.5, alpha = 0.2))
+  expect_gg(ppc_km_overlay(y2, yrep2, status_y = status_y2))
+})
+
+# Visual tests -----------------------------------------------------------------
+
+test_that("ppc_km_overlay renders correctly", {
+  testthat::skip_on_cran()
+  testthat::skip_if_not_installed("vdiffr")
+
+  p_base <- ppc_km_overlay(vdiff_y2, vdiff_yrep2, status_y = vdiff_status_y2)
+  vdiffr::expect_doppelganger("ppc_km_overlay (default)", p_base)
+
+  p_custom <- ppc_km_overlay(
+    vdiff_y2,
+    vdiff_yrep2,
+    status_y = vdiff_status_y2,
+    size = 2,
+    alpha = .2)
+  vdiffr::expect_doppelganger("ppc_km_overlay (size, alpha)", p_custom)
+})
