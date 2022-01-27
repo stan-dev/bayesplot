35abb528ac060c0618fb510710be48f33a08bf91
Säilynoja Teemu <teemu.sailynoja@gmail.com>
2022-01-27 11:55:46 +0200

    typesetting improvement

--- man-roxygen/args-pit-ecdf.R
+++ man-roxygen/args-pit-ecdf.R
@@ -1,7 +1,7 @@
 #' @param K An optional integer defining the number of equally spaced evaluation
-#' points for the ECDF. If the
-#' submitted PIT values are known to be discrete, this should be the number of
-#' discrete cases. Defaults to the smaller of length(y) and ncol(yrep).
+#' points for the ECDF. If the submitted PIT values are known to be discrete,
+#' this should be the number of the discrete cases. Defaults to the smaller of
+#' length(y) and ncol(yrep).
 #' @param prob The desired simultaneous coverage level of the bands around the
 #' ECDF. A value in (0,1).
 #' @param plot_diff A boolean defining wether to plot the difference between the
