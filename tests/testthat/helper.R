0752e7c6cf33dfd0372c5962a666f9480dea1a76
jgabry <jgabry@gmail.com>
2024-06-26 15:20:40 -0600

    Update helper.R

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,6 +1,7 @@
 # used to skip visual tests of SVGs on r-devel and r-oldrel
 # because they can be slightly different than r-release occasionally
 skip_if_not_r_release <- function() {
+  # first try just skipping on devel
   testthat::skip_if(isTRUE(Sys.getenv("R_VERSION_TYPE") == "devel"))
 
   #testthat::skip_if_not(isTRUE(Sys.getenv("R_VERSION_TYPE") == "release"))
