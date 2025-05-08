fced0094005d235d77e76956a762f91eebb367f6
jgabry <jgabry@gmail.com>
2025-05-08 10:08:06 -0600

    Reorder random number generation for testing data

--- tests/testthat/data-for-ppc-tests.R
+++ tests/testthat/data-for-ppc-tests.R
@@ -23,6 +23,12 @@ vdiff_yrep2 <- matrix(rpois(30 * 10, 1), ncol = 30, nrow = 10)
 vdiff_group2 <- rep_len(c(1,2), length.out = 30)
 vdiff_status_y2 <- rep_len(0:1, length.out = length(vdiff_y2))
 
+vdiff_loo_y <- rnorm(100, 30, 5)
+vdiff_loo_yrep <- matrix(rnorm(100 * 400, 30, 5), nrow = 400)
+vdiff_loo_lw <- vdiff_loo_yrep
+vdiff_loo_lw[] <- rnorm(100 * 400, -8, 2)
+
+
 vdiff_y3 <- rexp(50, rate = 0.2)
 vdiff_status_y3 <- rep_len(0:1, length.out = length(vdiff_y3))
 vdiff_group3 <- rep_len(c(1,2), length.out = 50)
@@ -39,8 +45,4 @@ for (i in 1:50) {
   vdiff_yrep3[, i] <- simulate_truncated_exp(10, rate, vdiff_left_truncation_y3[i])
 }
 
-vdiff_loo_y <- rnorm(100, 30, 5)
-vdiff_loo_yrep <- matrix(rnorm(100 * 400, 30, 5), nrow = 400)
-vdiff_loo_lw <- vdiff_loo_yrep
-vdiff_loo_lw[] <- rnorm(100 * 400, -8, 2)
 set.seed(seed = NULL)
